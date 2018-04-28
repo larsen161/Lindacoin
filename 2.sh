95e0ef3299bf11d62db13a7fe8999425f34720dd
larsen161 <stevelarsen@gmail.com>
2018-04-28 14:11:50 +0100

    minor formating and add bypass to last install

--- 2.sh
+++ 2.sh
@@ -1,5 +1,5 @@
 cd ~/
-sudo apt-get install -y pkg-config
+sudo apt-get -y install pkg-config
 sleep 5
 sudo apt-get -y install build-essential autoconf automake libtool libboost-all-dev libgmp-dev libssl-dev libcurl4-openssl-dev git
 sleep 5
@@ -7,7 +7,7 @@ sudo add-apt-repository ppa:bitcoin/bitcoin
 sleep 5
 sudo apt-get update
 sleep 5
-sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
 
 # Install and configure the machines firewall
 sudo apt-get install ufw
