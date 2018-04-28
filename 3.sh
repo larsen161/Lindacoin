c5456daaa29a70e3f89c635e9091be41ac38a95a
larsen161 <stevelarsen@gmail.com>
2018-04-28 13:49:14 +0100

    add bootstraping to the 3rd step and move cleanup here

--- 3.sh
+++ 3.sh
@@ -1,5 +1,6 @@
 cd ~/
 wget https://github.com/Lindacoin/Linda/releases/download/2.0.0.1/Unix.Lindad.v2.0.0.1g.tar.gz
+curl -L https://www.dropbox.com/sh/0s9apwcl0uhlpmy/AABysrq0PEosSaHQKGyxCTioa?dl=1 > LindaBootstrap.zip
 tar -xzvf Unix.Lindad.v2.0.0.1g.tar.gz -C /usr/local/bin/
 Lindad -daemon
 echo " "
@@ -15,15 +16,38 @@ echo " "
 echo " "
 sleep 30
 Lindad stop
+
+# Remove default files and extract the bootstrap
+cd ~/.Linda
+rm -rf database txleveldb blk0001.dat peers.dat
+unzip ~/LindaBootstrap.zip -d ~/.Linda
+
+# Move the Lind.conf file and promot user finalise with their details
 mv ~/Lindacoin/Linda.conf ~/.Linda/Linda.conf
 cd ~/
 sudo nano ~/.Linda/Linda.conf
+
+# Run the Linda wallet
+Lindad
+
+# Cleanup steps
+cd ~/
+rm -rf Lindacoin
+rm -rf https://github.com/Lindacoin/Linda/releases/download/2.0.0.1/Unix.Lindad.v2.0.0.1g.tar.gz
+rm ~/LindaBootstrap.zip
 echo " "
 echo " "
 echo " "
 echo " "
 echo " "
-echo "NerdyUser: Part 3 completed, go to step: sh 4.sh!"
+echo "NerdyUser: Setup Complete"
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "NerdyUser: Perform by yourself: cd ~/"
+echo "NerdyUser: before you continue"
 echo " "
 echo " "
 echo " "
