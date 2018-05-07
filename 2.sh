c604d187801ee470a540c834dc0c913572a16c70
larsen161 <stevelarsen@gmail.com>
2018-05-07 03:11:43 +0100

    latest bootstrap, linda.conf fixes, general cleanup

--- 2.sh
+++ 2.sh
@@ -1,9 +1,8 @@
-cd ~/
 # Install and configure the machines firewall
 sudo apt-get install fail2ban
 
 # Install any additional updates
-sudo apt update && sudo apt -y upgrade
+sudo apt update && sudo apt upgrade -y
 
 # Get the Linda binanry and extract
 wget https://github.com/Lindacoin/Linda/releases/download/2.0.0.1/Unix.Lindad.v2.0.0.1g.tar.gz
@@ -23,9 +22,9 @@ echo " "
 sleep 30
 Lindad stop
 
-# Get the bootstrap, remove default files then extract thbootstrap
+# Get the bootstrap, remove default files then extract the bootstrap
 cd ~/.Linda
-wget -O bootstrap.tar.gz https://transfer.sh/PfkAN/linda-v2-bootstrap-2018-05-05.tar.gz 
+wget -O bootstrap.tar.gz https://transfer.sh/bqhDi/linda-v2-bootstrap-2018-05-07.tar.gz 
 rm -rf database txleveldb blk0001.dat peers.dat
 tar xvf bootstrap.tar.gz
 
@@ -48,24 +47,19 @@ server=1
 listen=1
 daemon=1
 logtimestamps=1
-maxconnections=256
+maxconnections=128
 masternode=1
+masternodeaddr=${ipaddress}:33820
 masternodeprivkey=${masternodegenkey}
-addnode=seed1.linda-wallet.com
-addnode=seed2.linda-wallet.com
-addnode=seed3.linda-wallet.com
-addnode=seed4.linda-wallet.com
-addnode=seed5.linda-wallet.com
-addnode=seed6.linda-wallet.com
-addnode=seed7.linda-wallet.com
-addnode=seed8.linda-wallet.com
-addnode=seed9.linda-wallet.com
-addnode=seed10.linda-wallet.com
-addnode=seed11.linda-wallet.com" > ~/.Linda/Linda.conf
-
-mv ~/Lindacoin/Linda.conf ~/.Linda/Linda.conf
-cd ~/
-sudo nano ~/.Linda/Linda.conf
+seednode=seed1.linda-wallet.com
+seednode=seed2.linda-wallet.com
+seednode=seed3.linda-wallet.com
+seednode=seed4.linda-wallet.com
+seednode=seed5.linda-wallet.com
+seednode=seed6.linda-wallet.com
+seednode=seed7.linda-wallet.com
+seednode=seed8.linda-wallet.com
+seednode=seed9.linda-wallet.com" > ~/.Linda/Linda.conf
 
 # Cleanup steps
 cd ~/
@@ -73,7 +67,7 @@ rm -rf Lindacoin
 rm -rf https://github.com/Lindacoin/Linda/releases/download/2.0.0.1/Unix.Lindad.v2.0.0.1g.tar.gz
 rm ~/bootstrap.tar.gz
 
-# Run the Linda wallet
+# Run the Linda wallet, show Linda.conf
 Lindad
 echo " "
 echo " "
@@ -81,8 +75,10 @@ echo " "
 echo " "
 echo " "
 echo "Setup Complete"
+echo "Confirm configuration viewing Linda.conf below"
 echo " "
 echo " "
 echo " "
 echo " "
 echo " "
+cat ~/.Linda/Linda.conf
